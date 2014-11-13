b1b99277fbb416a6bdcf334cda43b97ea462f3c1
Max <makcumckopobogach@gmail.com>
2014-11-13 17:32:55 +0200

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,6 +1,4 @@
 echo 'Hello from' . <Mikhail Dyachuk>
-<<<<<<< HEAD
 echo 'Hello from' . <Alexandra Klimenko>
-=======
 echo 'Hello from' . <Skorobogach Maksim>
->>>>>>> aa6c648c4d843cc2a4218bc53acfb5855c005e94
+
