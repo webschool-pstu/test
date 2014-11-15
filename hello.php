987642b8da2968106738929a4874be771a9b1250
Никита Бескоровайный <huntguter@gmail.com>
2014-11-15 23:07:01 +0200

    Update by HuGe

--- hello.php
+++ hello.php
@@ -3,4 +3,4 @@ echo 'Hello from' . <Alexandra Klimenko>
 echo 'Hello from' . <Skorobogach Maksim>
 echo 'Hello from' . <Fabrichnuy Alexey>
 echo 'Hello from' . <Tanya Guseva>
-echo 'Hello from' . <Beskorovatniy Nikita>
+echo 'Hello from' . <Beskorovayniy Nikita>
