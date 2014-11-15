7c455c015da5e5be089fca62424649eb5e968b76
Никита Бескоровайный <huntguter@gmail.com>
2014-11-15 22:49:53 +0200

    Revert "test_Guseva"
    This reverts commit bfaa72121f29413db7afc5e8635cc949fe867a88.

--- hello.php
+++ hello.php
@@ -2,4 +2,3 @@ echo 'Hello from' . <Mikhail Dyachuk>
 echo 'Hello from' . <Alexandra Klimenko>
 echo 'Hello from' . <Skorobogach Maksim>
 echo 'Hello from' . <Fabrichnuy Alexey>
-echo 'Hello from' . <Tanya Guseva>
